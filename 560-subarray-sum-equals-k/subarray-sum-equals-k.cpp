8c573e6eb04a7d11abc48ff4f0fc2658dcef53f0
jay0x <90309873+blazie2004@users.noreply.github.com>
2026-05-19 03:50:57 +0530

    Time: 31 ms (95.69%) | Memory: 45.5 MB (38.48%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+
+        //range sum 
+        // p[j]-p[i-1]==k==p[i....j]
+        // so p here is prefix sum 
+        // we need count of all the subarrays 
+        //what we have p[j]-k if this sum is encountered before we have a range sum
+        // increase the count
+
+
+        unordered_map<int,int>mp;
+
+        int sum=0;
+        int count=0;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+
+            if(mp.find(sum-k)!=mp.end()){
+                //if present 
+                count=count+mp[sum-k];
+            }
+            
+            mp[sum]++;
+        }
+        return count;
+    }
+};
