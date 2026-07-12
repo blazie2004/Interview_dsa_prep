509e9a9b6dc8067ccee533192fe3b2685409b8b2
jay0x <90309873+blazie2004@users.noreply.github.com>
2026-07-12 17:28:00 +0530

    Time: 11 ms (47.32%) | Memory: 11.4 MB (65.57%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,61 +1,59 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>mp;
-        for(auto it:t){
-            mp[it]++;
-        }
-        int count=mp.size();
-        int n=s.length();
-        int i=0;
-        int j=0;
 
-        int idx=-1;
-        int idx2=-1;
-        int mini=INT_MAX;
-        while(j<n){
-            if(mp.find(s[j])!=mp.end()){
-                //we encountered the element present in t;
+        unordered_map<char,int> mp;
+
+        for (int i = 0; i < t.length(); i++) {
+            mp[t[i]]++;
+        }
+
+        int n = s.length();
+
+        int count = mp.size();
+        int mini_window = INT_MAX;
+
+        int idx1 = -1;
+        int idx2 = -1;
+
+        int i = 0;
+        int j = 0;
+
+        while (j < n) {
+
+            if (mp.find(s[j]) != mp.end()) {
                 mp[s[j]]--;
 
-                if(mp[s[j]]==0)count--;
-                //one element of a particular type got cancelled;
+                if (mp[s[j]] == 0)
+                    count--;
             }
 
-            //now when we will figure out the whole string t is encountered??
-            //once the count is 0;
+            while (count == 0) {
 
-            while(count==0){
-                //here at this jth position says this is position index of s where u can find whole string t;
-                int window_size=j-i+1;
+                int window = j - i + 1;
 
-                if(window_size<mini){
-                    idx=i;
-                    idx2=j;
-                    mini=window_size;
+                if (window < mini_window) {
+                    mini_window = window;
+                    idx1 = i;
+                    idx2 = j;
                 }
 
-                //store the string size 
-                //but its possible that we can find another same string t further in s
-                // we are hitting the case of string s found when count==0;
-                // for that increase the count in mp;
-                if(mp.find(s[i])!=mp.end()){
+                if (mp.find(s[i]) != mp.end()) {
                     mp[s[i]]++;
-                    if(mp[s[i]]>0){
+
+                    if (mp[s[i]] > 0)
                         count++;
-                    }
                 }
+
                 i++;
-
             }
+
             j++;
-
         }
-        
-        cout<<"min len substring"<<mini<<""<<endl;
 
-        if(mini==INT_MAX)return "";
+        if (idx1 == -1)
+            return "";
 
-        return s.substr(idx,idx2-idx+1);
+        return s.substr(idx1, mini_window);
     }
 };
