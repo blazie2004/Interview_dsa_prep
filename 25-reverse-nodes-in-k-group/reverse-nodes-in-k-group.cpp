f0fc79395e243d5e3569a8927b62cd7f581e6dd1
jay0x <90309873+blazie2004@users.noreply.github.com>
2026-07-12 16:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (71.00%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        
+        ListNode* temp = head;
+        for (int i = 0; i < k; i++) {
+            if (temp == NULL)
+                return head;
+            temp = temp->next;
+        }
+        ///base call kar do
+        //condition laga to stop rec
+
+        
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        int count = 0;
+
+        while (curr != NULL && count < k) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+
+        
+        head->next = reverseKGroup(curr, k);
+
+        return prev;
+    }
+};
